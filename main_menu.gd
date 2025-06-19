2c9d94cb3a5f0df2d94733fa910b4bf4c11d5ad9
finn8892 <159733074+finn8892@users.noreply.github.com>
2025-06-19 12:03:05 +1200

    yo

---
+++ main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://object.tscn")
+
+func _on_exit_pressed() -> void:
+	get_tree().exit()
+
+func _on_options_pressed() -> void:
+	print("options pressed")
